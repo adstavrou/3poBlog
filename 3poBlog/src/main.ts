0bfe27a9033abdbd5e43042c479ec90312dcacd6
Alexandros Stavrou <alexandros.stavrou@camelotls.com>
2021-09-12 14:30:02 +0300

    [3po-6 Firebase] attempt to connect with Firebase - failed

--- 3poBlog/src/main.ts
+++ 3poBlog/src/main.ts
@@ -2,17 +2,33 @@ import { createApp } from 'vue'
 import App from './App.vue'
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
-import { initializeApp } from "firebase/app";
+import { initializeApp } from 'firebase/app';
+import { getFirestore, collection, getDocs, addDoc } from 'firebase/firestore';
+
+const isLocalHost = location.hostname === 'localhost'
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCFD5cQZEmI8ddG2OlnN7GT7uhcZxqoHqU",
-    authDomain: "the3poblog.firebaseapp.com",
-    projectId: "the3poblog",
-    storageBucket: "the3poblog.appspot.com",
-    messagingSenderId: "546644442982",
-    appId: "1:546644442982:web:25dec0162006e91bf192a7"
-};
+    apiKey: 'AIzaSyCFD5cQZEmI8ddG2OlnN7GT7uhcZxqoHqU',
+    authDomain: 'the3poblog.firebaseapp.com',
+    databaseURL: isLocalHost
+                    ? 'http://localhost:9000/?ns=the3poblog-default-rtdb'
+                    : 'https://the3poblog-default-rtdb.europe-west1.firebasedatabase.app',
+    projectId: 'the3poblog',
+    storageBucket: 'the3poblog.appspot.com',
+    messagingSenderId: '546644442982',
+    appId: '1:546644442982:web:25dec0162006e91bf192a7'
+}
 
-const app = initializeApp(firebaseConfig);
+console.log(firebaseConfig)
 
-createApp(App).mount('#app')
+const fireApp = initializeApp(firebaseConfig);
+const db = getFirestore(fireApp);
+
+const query = await db.('siteConfiguration')
+
+createApp(App).mount('#app');
+
+
+
+
+console.log(query);
