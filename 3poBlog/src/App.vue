21a1fc1dcf7c1b00b58f9072f5544333e47a8640
Alexandros Stavrou <alexandros.stavrou@camelotls.com>
2021-10-15 18:58:39 +0300

    [3po-0 main] back to install, vite vue and sass

--- 3poBlog/src/App.vue
+++ 3poBlog/src/App.vue
@@ -1,21 +1,21 @@
-<template>
-  <Header></Header>
-</template>
-
-<script lang="ts">
-import { defineComponent } from 'vue'
-import Header from './components/header/Header.vue'
-
-export default defineComponent({
-  name: 'App',
-  components: {
-    Header
-  }
-})
+<script setup lang="ts">
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
+import HelloWorld from './components/HelloWorld.vue'
 </script>
+
+<template>
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
+</template>
 
 <style>
 #app {
-
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
 }
 </style>
--- 3poBlog/src/components/header/Header.vue
+++
@@ -1,3 +0,0 @@
-<template src="./header.html"></template>
-<style src="./header.scss"></style>
-<script type="module" src="./header.ts"></script>
