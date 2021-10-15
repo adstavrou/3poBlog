bd691e9950d0e0be567c136a418a33651ac83564
Alexandros Stavrou <alexandros.stavrou@camelotls.com>
2021-10-15 21:58:42 +0300

    [3po-1 main] firebase setup

--- 3poBlog/src/main.ts
+++ 3poBlog/src/main.ts
@@ -1,4 +1,54 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from 'vue';
+import App from './App.vue';
 
-createApp(App).mount('#app')
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getFirestore, collection, getDocs, getDoc, setDoc, doc } from 'firebase/firestore';
+import { getAnalytics } from "firebase/analytics";
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyD1Ny58iJlURrDstLhgT4STKp7VAtaHfRc",
+    authDomain: "poblog-af484.firebaseapp.com",
+    projectId: "poblog-af484",
+    storageBucket: "poblog-af484.appspot.com",
+    messagingSenderId: "142219538193",
+    appId: "1:142219538193:web:f53d3ea37c4a8fb0b7a6b0",
+    measurementId: "G-0S2S2LKXPK"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+//const analytics = getAnalytics(app);
+
+const db = getFirestore(app);
+
+async function getNavigation(db : any) {
+    const navigation = collection(db, 'navigation');
+    const navigationSnapshot = await getDocs(navigation);
+    const navigationList = navigationSnapshot.docs.map(doc => doc.data());
+    return navigationList;
+}
+
+await setDoc (doc(db, 'navigation', 'nav3'), {
+    text: 'Javascript',
+    url : 'javascript'
+});
+
+//console.log(getNavigation(db), collection(db, 'navigation'));
+
+const navItems = await getDocs(collection(db, 'navigation'));
+
+navItems.forEach((doc) => {
+    console.log(doc.id, " => ", doc.data());
+})
+
+console.log('test',navItems);
+
+
+
+createApp(App).mount('#app');
--- README.md
+++ README.md
@@ -3,7 +3,7 @@ A blog based on vue.js
 
 **commands:**
 
-- Vite Start as DEV: `yarn dev`
-- Vite Build project: `yarn build`
+- Vite Start as DEV: `npm run dev`
+- Vite Build project: `npm run build`
 - Firebase emulator Start: `firebase emulators:start`
 
