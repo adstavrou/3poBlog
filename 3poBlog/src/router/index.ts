8b239c5654c5a3922db531c7606ed6e290be5a24
Alexandros Stavrou <alexandros.stavrou@camelotls.com>
2022-01-31 14:05:13 +0200

    [3po-1 main] Rooter Lazy load components

--- 3poBlog/src/router/index.ts
+++ 3poBlog/src/router/index.ts
@@ -1,16 +1,16 @@
 import {createRouter, createWebHistory} from 'vue-router';
 
 import Home from '@/views/home/Home.vue'
-import Category from '@/views/category/Category.vue'
 
 const routes:Array<any> = [
     {path: '/', name: 'Home', component: Home},
     {path: '/category/:categoryName',
         name: 'Category',
-        component: Category,
-        children: [
-            {path: '', component: Category}
-        ]
+        component: () => import('@/views/category/Category.vue'),
+        children: [{
+            path: '',
+            component: () => import('@/views/category/Category.vue')
+        }]
     }
 ];
 
